cd5c2b885451ada948a7189ffd2eea8d670912d4
Rüzgar Mehmet Deniz <ruzgarmehmetdeniz@gmail.com>
2026-02-11 18:25:49 +0300

    .

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/CarHandlers/RemoveCarCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Commands.CarCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class RemoveCarCommandHandler
+    {
+        private readonly IRepository<Car> _repositories;
+
+        public RemoveCarCommandHandler(IRepository<Car> repositories)
+        {
+            _repositories = repositories;
+        }
+        public async Task Handle (RemoveCarCommand command)
+        {
+            var value = await _repositories.GetByIdAsync(command.Id);
+            await _repositories.RemoveAsync(value);
+
+        }
+    }
+}
